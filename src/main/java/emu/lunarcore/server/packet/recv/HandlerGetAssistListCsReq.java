3bfaf46e3ba2e46b5c3502a4f34173584735060f
Melledy <121644117+Melledy@users.noreply.github.com>
2023-11-01 13:12:28 -0700

    Add simple handler for `GetAssistListCsReq`

---
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerGetAssistListCsReq.java
@@ -0,0 +1,16 @@
+package emu.lunarcore.server.packet.recv;
+
+import emu.lunarcore.server.game.GameSession;
+import emu.lunarcore.server.packet.CmdId;
+import emu.lunarcore.server.packet.Opcodes;
+import emu.lunarcore.server.packet.PacketHandler;
+
+@Opcodes(CmdId.GetAssistListCsReq)
+public class HandlerGetAssistListCsReq extends PacketHandler {
+
+    @Override
+    public void handle(GameSession session, byte[] header, byte[] data) throws Exception {
+        session.send(CmdId.GetAssistListScRsp);
+    }
+
+}
