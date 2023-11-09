03d2ff71861497144aba563491ba3f42f88e9b26
Melledy <121644117+Melledy@users.noreply.github.com>
2023-11-08 21:57:22 -0800

    Implement hcoin exchange handler

---
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerExchangeHcoinCsReq.java
@@ -0,0 +1,25 @@
+package emu.lunarcore.server.packet.recv;
+
+import emu.lunarcore.proto.ExchangeHcoinCsReqOuterClass.ExchangeHcoinCsReq;
+import emu.lunarcore.server.game.GameSession;
+import emu.lunarcore.server.packet.CmdId;
+import emu.lunarcore.server.packet.Opcodes;
+import emu.lunarcore.server.packet.PacketHandler;
+
+@Opcodes(CmdId.ExchangeHcoinCsReq)
+public class HandlerExchangeHcoinCsReq extends PacketHandler {
+
+    @Override
+    public void handle(GameSession session, byte[] header, byte[] data) throws Exception {
+        var req = ExchangeHcoinCsReq.parseFrom(data);
+        
+        if (session.getPlayer().getMcoin() >= req.getNum()) {
+            session.getPlayer().addMCoin(-req.getNum());
+            session.getPlayer().addHCoin(req.getNum());
+            session.getPlayer().save();
+        }
+        
+        session.send(CmdId.ExchangeHcoinScRsp);
+    }
+
+}
