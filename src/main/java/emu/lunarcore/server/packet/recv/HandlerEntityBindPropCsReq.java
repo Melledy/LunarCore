e6f65f47f53901bca27f5ce8d4de247e9c086cb0
Melledy <121644117+Melledy@users.noreply.github.com>
2023-12-07 03:00:57 -0800

    Optimize handler for `GetMainMissionCustomValueCsReq`

--- src/main/java/emu/lunarcore/server/packet/recv/HandlerEntityBindPropCsReq.java
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerEntityBindPropCsReq.java
@@ -1,7 +1,6 @@
 package emu.lunarcore.server.packet.recv;
 
 import emu.lunarcore.server.game.GameSession;
-import emu.lunarcore.server.packet.BasePacket;
 import emu.lunarcore.server.packet.CmdId;
 import emu.lunarcore.server.packet.Opcodes;
 import emu.lunarcore.server.packet.PacketHandler;
@@ -11,7 +10,7 @@ public class HandlerEntityBindPropCsReq extends PacketHandler {
 
     @Override
     public void handle(GameSession session, byte[] data) throws Exception {
-        session.send(new BasePacket(CmdId.EntityBindPropScRsp));
+        session.send(CmdId.EntityBindPropScRsp);
     }
 
 }
